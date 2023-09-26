25eec4790aae6cea02228a16b540f439e318ff6e
Pratikshaa1216 <129977956+Pratikshaa1216@users.noreply.github.com>
2023-09-26 09:03:34 +0200

    autojs.py

--- CodeGreen_Javascript/autojs.py
+++ CodeGreen_Javascript/autojs.py
@@ -4,40 +4,41 @@ from codecarbon import EmissionsTracker
 import psutil
 
 def start_web_application():
-   
+    # Start a web server using the 'http.server' module on port 8080
     subprocess.Popen(["python", "-m", "http.server", "8080"])
 
 def open_browser(url):
-   
+    # Open a web browser with the specified URL
     subprocess.Popen(["start", "", url], shell=True)
 
 def track_emissions(duration):
+    # Create an instance of the EmissionsTracker to monitor emissions
     tracker = EmissionsTracker()
-    tracker.start()
+    tracker.start()  # Start tracking emissions
 
-   
+    # Sleep for the specified duration to collect emissions data
     time.sleep(duration)
 
-    tracker.stop()
-    tracker.save_to_csv("emissions.csv")
+    tracker.stop()  # Stop tracking emissions
+    tracker.save_to_csv("emissions.csv")  # Save emissions data to a CSV file
 
 def shutdown_web_application():
- 
-    for proc in psutil.process_iter(['pid', 'name']):
+  for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == 'python' and '-m http.server 8080' in ' '.join(proc.cmdline()):
             print("Web application process:", proc.info['name'], proc.info['pid'])
-            proc.terminate()
+            proc.terminate()  
 
 def main():
-    duration = 90 
+    duration = 90  # Duration (in seconds) for which to track emissions
 
-    start_web_application()
-    open_browser("http://127.0.0.1:8080/index.html")  
-    track_emissions(duration)
+    start_web_application()  # Start the web server
+    open_browser("http://127.0.0.1:8080/index.html")  # Open the web page in a browser
+    track_emissions(duration)  # Track emissions during the specified duration
 
-    shutdown_web_application()
+    shutdown_web_application()  # Shutdown the web server process
 
 if __name__ == '__main__':
-    main()
+    main()  
+
 
 
